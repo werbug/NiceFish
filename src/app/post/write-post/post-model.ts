9efa5d4021fcb86fbe79d038f33738dcfadf8248
zhang.xiaofei <damoqiongqiu@126.com>
2017-05-04 22:19:49 +0800

    加2个文件；

---
+++ src/app/post/write-post/post-model.ts
@@ -0,0 +1,6 @@
+export class Post {
+	postId:String;
+	postTitle:String;
+	originalURL:String;
+	content:String;
+}
---
+++ src/app/post/write-post/write-post.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions,URLSearchParams } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import { Post } from './post-model';
+
+@Injectable()
+export class WritePostService {
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  public writePostURL = 'post/newPost';
+
+  constructor(public http:Http) { }
+  
+  public newPost(post:Post):Observable<any>{
+    return this.http
+          .post(this.writePostURL,JSON.stringify(post), {headers: this.headers})
+          .map((res: Response) => {
+            let result = res.json();
+            console.log(result);
+            return result;
+          });
+  }
+}
